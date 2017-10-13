3f4e9077d20a1d9c9aae966c6e8d54e8dd3469fc
ld <liding2016@ia.ac.cn>
2017-10-13 10:18:42 +0800

    Test the connection.

---
+++ test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: liding
+@license: Apache Licence 
+@contact: liding2016@ia.ac.cn
+@site: 
+@file: test.py
+@time: 2017/10/13 10:08
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
